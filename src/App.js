21f2224a2cbe8b669fcf8e47a646230ef68bba24
Tanjimul Islam <tanjimulislam118@gmail.com>
2021-12-23 16:07:54 +0600

    fix: endpoint re-written and getting element from pages folder instead of components folder

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Vendor, Type, Category, Unit, Medicine, Purchase, Customer, Bill, Dashboard } from './components'
 import { SidebarProvider } from './contexts/sidebarContext'
+import { Home, Vendor, Bill, Customer, Medicine } from './pages'
 
 function App() {
     return (
         <SidebarProvider>
             <BrowserRouter>
                 <Routes>
-                    <Route exact path="/" element={<Dashboard />} />
-                    <Route path="/add_vendor" element={<Vendor />} />
-                    <Route path="/add_type" element={<Type />} />
-                    <Route path="/add_category" element={<Category />} />
-                    <Route path="/add_unit" element={<Unit />} />
-                    <Route path="/add_medicine" element={<Medicine />} />
-                    <Route path="/add_purchase" element={<Purchase />} />
-                    <Route path="/add_customer" element={<Customer />} />
-                    <Route path="/add_bill" element={<Bill />} />
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/vendor" element={<Vendor />} />
+                    <Route path="/medicine" element={<Medicine />} />
+                    <Route path="/customer" element={<Customer />} />
+                    <Route path="/bill" element={<Bill />} />
                 </Routes>
             </BrowserRouter>
         </SidebarProvider>
