17a8a2791195804474687108edc6d267ef949d7a
Morshedul Bari Antor <morshedul.antor@northsouth.edu>
2021-12-25 02:53:32 +0600

    refactor: - add medicine modal form with css

--- src/components/Medicine/MedicineForm/MedicineForm.jsx
+++ src/components/Medicine/MedicineForm/MedicineForm.jsx
@@ -1,45 +1,107 @@
+import { Link } from 'react-router-dom'
+import classes from './MedicineForm.module.css'
+
 export default function MedicineForm({ setIsOpenForm }) {
     return (
-        <form className={classes.item} onSubmit={handleSubmit}>
-            <h2>Add New Medicine</h2>
-            <input placeholder="name" name="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-            <label htmlFor="types">Select type</label>
-            <select onChange={(e) => setTypeId(e.target.value)} id="types">
-                <option value="">select</option>
-                {types.map((type, i) => (
-                    <option key={i} value={type.id}>
-                        {type.name}
-                    </option>
-                ))}
-            </select>
-            <label htmlFor="categories">Select category</label>
-            <select onChange={(e) => setCategorieId(e.target.value)} id="categories">
-                <option value="">select</option>
-                {categories.map((category, i) => (
-                    <option key={i} value={category.id}>
-                        {category.name}
-                    </option>
-                ))}
-            </select>
-            <label htmlFor="units">Select unit</label>
-            <select onChange={(e) => setUnitId(e.target.value)} id="units">
-                <option value="">select</option>
-                {units.map((unit, i) => (
-                    <option key={i} value={unit.id}>
-                        {unit.name}
-                    </option>
-                ))}
-            </select>
-            <label htmlFor="vendors">Select vendor</label>
-            <select onChange={(e) => setVendorId(e.target.value)} id="vendors">
-                <option value="">select</option>
-                {vendors.map((vendor, i) => (
-                    <option key={i} value={vendor.id}>
-                        {vendor.name}
-                    </option>
-                ))}
-            </select>
-            <button type="submit">Submit</button>
-        </form>
+        <div className={classes.overlay} id="popup">
+            <div className={classes.formWrapper}>
+                <h2>Add New Medicine</h2>
+                <Link className={classes.close} to="./">
+                    &times;
+                </Link>
+
+                <form classNameName={classes.item} onSubmit={handleSubmit}>
+                    <div className={classes.inputbox}>
+                        <input type="text" name="" required="" />
+                        <label htmlFor="medicines">Medicine Name</label>
+                    </div>
+
+                    <div className={classes.inputbox}>
+                        <input name="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                        <label htmlFor="types">Select Type</label>
+                        <select onChange={(e) => setTypeId(e.target.value)} id="types">
+                            <option value="">select</option>
+                            {types.map((type, i) => (
+                                <option key={i} value={type.id}>
+                                    {type.name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <div className={classes.inputbox}>
+                        <label htmlFor="categories">Select Category</label>
+                        <select onChange={(e) => setCategorieId(e.target.value)} id="categories">
+                            <option value="">select</option>
+                            {categories.map((category, i) => (
+                                <option key={i} value={category.id}>
+                                    {category.name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <div className={classes.inputbox}>
+                        <label htmlFor="units">Select Unit</label>
+                        <select onChange={(e) => setUnitId(e.target.value)} id="units">
+                            <option value="">select</option>
+                            {units.map((unit, i) => (
+                                <option key={i} value={unit.id}>
+                                    {unit.name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <div className={classes.inputbox}>
+                        <label htmlFor="vendors">Select Vendor</label>
+                        <select onChange={(e) => setVendorId(e.target.value)} id="vendors">
+                            <option value="">select</option>
+                            {vendors.map((vendor, i) => (
+                                <option key={i} value={vendor.id}>
+                                    {vendor.name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <button className={classes.button}>Submit</button>
+                </form>
+            </div>
+        </div>
     )
 }
+
+/* return (
+        <div className={classes.overlay} id="popup">
+            <div className={classes.formWrapper}>
+                <h2>Add Medicine</h2>
+                <Link className={classes.close} to="#">
+                    &times;
+                </Link>
+                <form>
+                    <div className={classes.inputbox}>
+                        <input type="text" name="" required="" />
+                        <label>Medicine Name</label>
+                    </div>
+                    <div className={classes.inputbox}>
+                        <input type="text" name="" required="" />
+                        <label>Type</label>
+                    </div>
+                    <div className={classes.inputbox}>
+                        <input type="text" name="" required="" />
+                        <label>Company</label>
+                    </div>
+                    <div className={classes.inputbox}>
+                        <input type="text" name="" required="" />
+                        <label>Stock</label>
+                    </div>
+                    <div className={classes.inputbox}>
+                        <input type="text" name="" required="" />
+                        <label>Unit</label>
+                    </div>
+                    <button className={classes.button}>Submit</button>
+                </form>
+            </div>
+        </div>
+    ) */
