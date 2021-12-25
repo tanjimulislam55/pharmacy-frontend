20b3fe0f8de587c640f1734d24d2b7a31273b8e5
tanjimulislam55 <tanjimulislam118@gmail.com>
2021-12-25 22:02:49 +0600

    fix: removed context provider was written twice

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,18 +1,18 @@
-import { SidebarProvider } from '../../contexts/sidebarContext'
 import classes from './Layout.module.css'
 import Navbar from './Navbar/Navbar'
 import Sidebar from './Sidebar/Sidebar'
+import { SidebarContext } from '../../contexts/sidebarContext'
+import { useContext } from 'react'
 
 export default function Layout({ children }) {
+    const { isOpenSidebar } = useContext(SidebarContext)
     return (
-        <SidebarProvider>
-            <div className={classes.layout}>
-                <Sidebar />
-                <div className={classes.container}>
-                    <Navbar />
-                    {children}
-                </div>
+        <div className={!isOpenSidebar ? classes.layout: classes.layoutWhenSidebarIsHidden}>
+            <Sidebar />
+            <div className={classes.container}>
+                <Navbar />
+                {children}
             </div>
-        </SidebarProvider>
+        </div>
     )
 }
