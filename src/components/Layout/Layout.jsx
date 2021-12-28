54a0027a100c470f32fb287739f81c9f6f061185
Tanjimul Islam <tanjimulislam118@gmail.com>
2021-12-28 15:58:29 +0600

    prettier line allighnment

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,13 @@
+import { useContext } from 'react'
+import { SidebarContext } from '../../contexts/sidebarContext'
 import classes from './Layout.module.css'
 import Navbar from './Navbar/Navbar'
 import Sidebar from './Sidebar/Sidebar'
-import { SidebarContext } from '../../contexts/sidebarContext'
-import { useContext } from 'react'
 
 export default function Layout({ children }) {
     const { isOpenSidebar } = useContext(SidebarContext)
     return (
-        <div className={!isOpenSidebar ? classes.layout: classes.layoutWhenSidebarIsHidden}>
+        <div className={!isOpenSidebar ? classes.layout : classes.layoutWhenSidebarIsHidden}>
             <Sidebar />
             <div className={classes.container}>
                 <Navbar />
