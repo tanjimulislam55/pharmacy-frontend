6bf55b7e2be8e3d6f7e2cc0e08c0c5f8d90b1414
morshedul-antor <morshedul.antor@northsouth.edu>
2022-01-02 16:38:04 +0600

    feat: add dashboard item display list feature with module css

---
+++ src/components/Dashboard/DashboardItem/DashboardItem.jsx
@@ -0,0 +1,40 @@
+import { faAddressBook, faCapsules, faHandHoldingMedical, faLaptopMedical } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import classes from './DashboardItem.module.css'
+
+const DashboardItem = () => {
+    return (
+        <div className={classes.dashContainer}>
+            <div className={classes.dashBox}>
+                <div className={classes.itemNumber}>10000</div>
+                <div className={classes.itemName}>Medicines</div>
+
+                <div className={classes.itemIcon}>
+                    <FontAwesomeIcon icon={faCapsules} />
+                </div>
+            </div>
+            <div className={classes.dashBox}>
+                <div div className={classes.itemNumber}>
+                    5000
+                </div>
+                <div className={classes.itemName}>Stock</div>
+
+                <div className={classes.itemIcon}>
+                    <FontAwesomeIcon icon={faHandHoldingMedical} />
+                </div>
+            </div>
+            <div className={classes.dashBox}>
+                <div div className={classes.itemNumber}>
+                    2000
+                </div>
+                <div className={classes.itemName}>Today's Sales</div>
+
+                <div className={classes.itemIcon}>
+                    <FontAwesomeIcon icon={faLaptopMedical} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DashboardItem
