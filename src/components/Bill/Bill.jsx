820bf1e139cfd61c6f8a77cf2c626a2a3237c6b8
Tanjimul Islam <tanjimulislam118@gmail.com>
2022-01-04 16:42:31 +0600

    feat: data fetched and send to billList component

--- src/components/Bill/Bill.jsx
+++ src/components/Bill/Bill.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from 'react'
+import BillForm from './BillForm/BillForm'
 import BillList from './BillList/BillList'
 
 export default function Bill() {
-    return <BillList />
+    const [bills, setBills] = useState([])
+    const [isOpenForm, setIsOpenForm] = useState(false)
+
+    useEffect(() => {
+        const controller = new AbortController()
+        const fetchData = async () => {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/bills`)
+            const data = await response.json()
+            setBills(data)
+        }
+        fetchData()
+        return () => {
+            fetchData()
+            controller.abort()
+        }
+    }, [])
+
+    return (
+        <div>
+            {isOpenForm && <BillForm />}
+            <BillList bills={bills} setIsOpenForm={setIsOpenForm} />
+        </div>
+    )
 }
