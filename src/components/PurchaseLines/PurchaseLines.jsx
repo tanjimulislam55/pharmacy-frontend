d4ef5a5f99a120c86a7b58d75c9d1facba046f4c
morshedul-antor <morshedul.antor@northsouth.edu>
2022-01-10 22:54:14 +0600

    add: css classes added

--- src/components/PurchaseLines/PurchaseLines.jsx
+++ src/components/PurchaseLines/PurchaseLines.jsx
@@ -28,44 +28,61 @@ export default function PurchaseLines({ purchaseLine, i, total, setTotal }) {
     }
 
     return (
-        <div key={i} className={classes.purchase_lines}>
-            <label htmlFor="categories">Select Medicine</label>
-            <select onChange={(e) => (purchaseLine.medicine_id = e.target.value)} id="medicines">
-                <option value="">select</option>
-                {medicines &&
-                    medicines.map((medicine, i) => (
-                        <option key={i} value={medicine.id}>
-                            {medicine.name}
-                        </option>
-                    ))}
-            </select>
-            <label htmlFor="purchasePrice">Purchase price</label>
-            <input
-                id="purchasePrice"
-                name="purchasePrice"
-                type="number"
-                value={purchaseLine.purchase_price}
-                onChange={(e) => (purchaseLine.purchase_price = e.target.value)}
-                onBlur={(e) => handleBlur(e)}
-            />
-            <label htmlFor="purchasedQuantiy">Purchased quantity</label>
-            <input
-                id="purchaseQuantiy"
-                name="purchaseQuantiy"
-                type="number"
-                value={purchaseLine.purchased_qty}
-                onChange={(e) => (purchaseLine.purchased_qty = e.target.value)}
-                onBlur={handleBlur}
-            />
-            <label htmlFor="receivedQuantity">Received quantity</label>
-            <input
-                id="receivedQuantity"
-                name="receivedQuantity"
-                type="number"
-                value={purchaseLine.received_qty}
-                onChange={(e) => (purchaseLine.received_qty = e.target.value)}
-            />
-            <div>Total: {total}</div>
+        <div className={classes.formWrapper}>
+            <div key={i}>
+                {/* <label htmlFor="categories">Select Medicine</label> */}
+                <div className={classes.selectBox}>
+                    <select
+                        className={classes.option}
+                        onChange={(e) => (purchaseLine.medicine_id = e.target.value)}
+                        id="medicines">
+                        <option value="">Select Medicine</option>
+                        {medicines &&
+                            medicines.map((medicine, i) => (
+                                <option key={i} value={medicine.id}>
+                                    {medicine.name}
+                                </option>
+                            ))}
+                    </select>
+                </div>
+                <div className={classes.inputbox}>
+                    <input
+                        id="purchasePrice"
+                        name="purchasePrice"
+                        type="number"
+                        value={purchaseLine.purchase_price}
+                        onChange={(e) => (purchaseLine.purchase_price = e.target.value)}
+                        onBlur={(e) => handleBlur(e)}
+                        required
+                    />
+                    <label htmlFor="purchasePrice">Purchase Price</label>
+                </div>
+
+                <div className={classes.inputbox}>
+                    <input
+                        id="purchaseQuantiy"
+                        name="purchaseQuantiy"
+                        type="number"
+                        value={purchaseLine.purchased_qty}
+                        onChange={(e) => (purchaseLine.purchased_qty = e.target.value)}
+                        onBlur={handleBlur}
+                        required
+                    />
+                    <label htmlFor="purchasedQuantiy">Purchased quantity</label>
+                </div>
+                <div className={classes.inputbox}>
+                    <input
+                        id="receivedQuantity"
+                        name="receivedQuantity"
+                        type="number"
+                        value={purchaseLine.received_qty}
+                        onChange={(e) => (purchaseLine.received_qty = e.target.value)}
+                        required
+                    />
+                    <label htmlFor="receivedQuantity">Received Quantity</label>
+                </div>
+                <div>Total: {total}</div>
+            </div>
         </div>
     )
 }
