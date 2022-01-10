b7f2a48c362dba67b36db16598329d0e2db7695f
morshedul-antor <morshedul.antor@northsouth.edu>
2022-01-10 17:23:31 +0600

    feat: add expense page

---
+++ src/pages/Expense.jsx
@@ -0,0 +1,11 @@
+import { Layout, Expense } from '../components'
+
+export default function ExpensePage() {
+    return (
+        <div>
+            <Layout>
+                <Expense />
+            </Layout>
+        </div>
+    )
+}
