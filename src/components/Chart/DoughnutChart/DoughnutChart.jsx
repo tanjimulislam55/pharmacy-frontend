ba146dbd0c85b9f61a066dd310f6f0786911f516
morshedul-antor <morshedul.antor@northsouth.edu>
2022-01-18 23:50:25 +0600

    add: top madicine value added

--- src/components/Chart/DoughnutChart/DoughnutChart.jsx
+++ src/components/Chart/DoughnutChart/DoughnutChart.jsx
@@ -5,17 +5,18 @@ import classes from './DougnutChart.module.css'
 ChartJS.register(ArcElement, Tooltip, Legend)
 
 export default function DoughnutChart() {
+    const labels = ['Napa Extra', 'Ace Plus', 'Antazol', 'Others']
     const data = {
-        labels: ['Red', 'Blue', 'Yellow'],
+        labels: labels,
         datasets: [
             {
-                data: [300, 50, 100],
-                backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+                label: 'Top Medicine',
+                data: [45, 25, 20, 10],
+                backgroundColor: ['#41add9', '#3ecbcf', '#2cbdb3', '#5cb484', '#7eb88b'],
                 hoverOffset: 1,
             },
         ],
     }
-
     return (
         <div className={classes.container}>
             <Doughnut data={data} />
