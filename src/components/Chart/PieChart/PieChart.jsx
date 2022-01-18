a02daa0f409030fcd44efba7f570e58b26d8a247
morshedul-antor <morshedul.antor@northsouth.edu>
2022-01-18 23:50:57 +0600

    add: top customer value added

--- src/components/Chart/PieChart/PieChart.jsx
+++ src/components/Chart/PieChart/PieChart.jsx
@@ -1,30 +1,28 @@
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Pie } from "react-chartjs-2";
-import classes from "./PieChart.module.css";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Pie } from 'react-chartjs-2'
+import classes from './PieChart.module.css'
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip, Legend)
 
 const PieChart = () => {
+    const labels = ['Sajid', 'Rishad', 'Antor', 'Others']
     const data = {
-        labels: ["Red", "Blue", "Yellow"],
+        labels: labels,
         datasets: [
             {
-                data: [300, 50, 100],
-                backgroundColor: [
-                    "rgb(255, 99, 132)",
-                    "rgb(54, 162, 235)",
-                    "rgb(255, 205, 86)",
-                ],
+                label: 'Top Customer',
+                data: [45, 20, 25, 10],
+                backgroundColor: ['#41add9', '#2cbdb3', '#5cb484', '#325fb1'],
                 hoverOffset: 1,
             },
         ],
-    };
+    }
 
     return (
         <div className={classes.container}>
             <Pie data={data} />
         </div>
-    );
-};
+    )
+}
 
-export default PieChart;
+export default PieChart
