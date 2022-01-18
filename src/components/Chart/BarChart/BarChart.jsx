d74ea9922db849aaa63008e16a87ac412e8cc5e6
morshedul-antor <morshedul.antor@northsouth.edu>
2022-01-18 23:49:39 +0600

    add: weekly value added

--- src/components/Chart/BarChart/BarChart.jsx
+++ src/components/Chart/BarChart/BarChart.jsx
@@ -1,30 +1,28 @@
-import { Chart as ChartJS, BarElement } from "chart.js";
-import { Bar } from "react-chartjs-2";
-import classes from "./BarChart.module.css";
+import { Chart as ChartJS, BarElement } from 'chart.js'
+import { Bar } from 'react-chartjs-2'
+import classes from './BarChart.module.css'
 
-ChartJS.register(BarElement);
+ChartJS.register(BarElement)
 
 const BarChart = () => {
+    const labels = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     const data = {
-        labels: ["Red", "Blue", "Yellow"],
+        labels: labels,
         datasets: [
             {
-                data: [300, 50, 100],
-                backgroundColor: [
-                    "rgb(255, 99, 132)",
-                    "rgb(54, 162, 235)",
-                    "rgb(255, 205, 86)",
-                ],
+                label: 'Weekly Sales',
+                data: [300, 450, 350, 500, 700, 400, 600, 800],
+                backgroundColor: ['#457dc3', '#41add9', '#3ecbcf', '#2cbdb3', '#5cb484', '#7eb88b'],
                 hoverOffset: 1,
             },
         ],
-    };
+    }
 
     return (
         <div className={classes.container}>
             <Bar data={data} />
         </div>
-    );
-};
+    )
+}
 
-export default BarChart;
+export default BarChart
