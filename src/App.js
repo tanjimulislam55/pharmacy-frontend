d52ea244e826559048b54e7bd523accc746c7e25
morshedul-antor <morshedul.antor@northsouth.edu>
2022-01-30 00:23:40 +0600

    add: route added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
+import { useReducer } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Auth, UserInfo } from './contexts/allContex'
+import { authReducer, authState } from './contexts/reducer/authReducer'
+import { userReducer, userState } from './contexts/reducer/userReducer'
 import { SidebarProvider } from './contexts/sidebarContext'
-import { Home, Vendor, Bill, Customer, Medicine, Stock, Expense, Purchase, Report, Admin } from './pages'
+import {
+    Home,
+    Vendor,
+    Bill,
+    Customer,
+    Medicine,
+    Stock,
+    Expense,
+    Purchase,
+    Report,
+    Admin,
+    BillForm,
+    PurchaseForm,
+    Login,
+    Registration,
+} from './pages'
+import ProtectedRoute from './routes/ProtectedRoute'
 
 function App() {
+    const [stateAuth, dispatchAuth] = useReducer(authReducer, authState)
+    const [stateUser, dispatchUser] = useReducer(userReducer, userState)
+
     return (
         <SidebarProvider>
             <BrowserRouter>
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route path="/vendor" element={<Vendor />} />
-                    <Route path="/medicine" element={<Medicine />} />
-                    <Route path="/customer" element={<Customer />} />
-                    <Route path="/bill" element={<Bill />} />
-                    <Route path="/stock" element={<Stock />} />
-                    <Route path="/expense" element={<Expense />} />
-                    <Route path="/purchase" element={<Purchase />} />
-                    <Route path="/purchase" element={<Purchase />} />
-                    <Route path="/reports" element={<Report />} />
-                    <Route path="/accounts" element={<Admin />} />
-                </Routes>
+                <Auth.Provider value={{ stateAuth, dispatchAuth }}>
+                    <UserInfo.Provider value={{ stateUser, dispatchUser }}>
+                        <Routes>
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/registration" element={<Registration />} />
+
+                            <Route path="/" element={<Home />} />
+                            <Route path="/medicine" element={<Medicine />} />
+                            <Route path="/vendor" element={<Vendor />} />
+                            <Route path="/customer" element={<Customer />} />
+                            <Route path="/bill" element={<Bill />} />
+                            <Route path="/bill/billform" element={<BillForm />} />
+                            <Route path="/stock" element={<Stock />} />
+                            <Route path="/expense" element={<Expense />} />
+                            <Route path="/purchase" element={<Purchase />} />
+                            <Route path="/purchase/purchaseform" element={<PurchaseForm />} />
+                            <Route path="/reports" element={<Report />} />
+                            <Route path="/accounts" element={<Admin />} />
+                        </Routes>
+                    </UserInfo.Provider>
+                </Auth.Provider>
             </BrowserRouter>
         </SidebarProvider>
     )
