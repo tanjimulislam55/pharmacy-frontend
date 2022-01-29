85141c5d0442e48d171efd4acd655d7e80587197
morshedul-antor <morshedul.antor@northsouth.edu>
2022-01-30 00:06:49 +0600

    feat: protected route added

---
+++ src/routes/ProtectedRoute.js
@@ -0,0 +1,32 @@
+import { useContext, useEffect } from 'react'
+import { Route, Navigate, Redirect } from 'react-router-dom'
+import { Auth, UserInfo } from '../contexts/allContex'
+
+export default function ProtectedRoute({ component: Component }) {
+    const { stateAuth } = useContext(Auth)
+    const { dispatchUser } = useContext(UserInfo)
+
+    const api = process.env.REACT_APP_API_URL
+
+    useEffect(() => {
+        let funFetch = async () => {
+            let logFetch = await fetch(`${api}/users/me`, {
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${stateAuth.token}`,
+                },
+                dataType: 'json',
+                method: 'GET',
+            })
+
+            let log = await logFetch.json()
+            dispatchUser({ type: 'set', payload: log })
+        }
+        if (stateAuth.auth === true) {
+            funFetch()
+        }
+    }, [stateAuth, dispatchUser, api])
+
+    return stateAuth.auth === true ? <Component /> : <Navigate to="/login" />
+}
