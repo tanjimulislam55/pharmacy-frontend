a4c03b04f3d605779a38de60d6b0d972099cc8af
morshedul-antor <morshedul.antor@northsouth.edu>
2022-01-30 00:08:56 +0600

    page: purchaseform page added

---
+++ src/pages/PurchaseForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Layout, PurchaseForm } from '../components'
+
+export default function PurchaseFormPage() {
+    return (
+        <Layout>
+            <PurchaseForm />
+        </Layout>
+    )
+}
