cc54898afb41af3e285f227f29937073e876e8fd
morshedul-antor <morshedul.antor@northsouth.edu>
2022-01-30 00:08:30 +0600

    page: billform page added

---
+++ src/pages/BillForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BillForm, Layout } from '../components'
+
+export default function BillFormPage() {
+    return (
+        <Layout>
+            <BillForm />
+        </Layout>
+    )
+}
