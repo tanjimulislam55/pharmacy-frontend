542f0d782e4be70aa72a9cc62d788f204ed3adc4
morshedul-antor <morshedul.antor@northsouth.edu>
2022-01-30 00:32:13 +0600

    feat: list fetch from backend and design with css

--- src/components/Purchase/PurchaseList/PurchaseList.jsx
+++ src/components/Purchase/PurchaseList/PurchaseList.jsx
@@ -1,8 +1,15 @@
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Pagination } from '../../../components'
 import classes from './PurchaseList.module.css'
 
-export default function PurchaseList() {
+export default function PurchaseList({ purchases }) {
+    const [searched, setSearched] = useState('')
+    const [startIdx, setStartIdx] = useState(0)
+    const [endIdx, setEndIdx] = useState(10)
+
     return (
         <div className={classes.tableContainer}>
             <div className={classes.wrapper}>
@@ -12,25 +19,40 @@ export default function PurchaseList() {
                         <FontAwesomeIcon icon={faSearch} />
                     </button>
                 </form>
-                <button className={classes.button}>+ New Purchase</button>
+                <button className={classes.button}>
+                    <Link to="./purchaseform">+ New Purchase</Link>
+                </button>
             </div>
 
             <table className={classes.tableMain}>
                 <tr className={classes.tableRow}>
-                    <th>Sl</th>
+                    <th>ID</th>
                     <th>Suppiler Name</th>
-                    <th>Amount</th>
-                    <th>Method</th>
+                    <th>Total Amount</th>
+                    <th>Due Amount</th>
+                    <th>Paid Amount</th>
                     <th>Date</th>
                 </tr>
-                <tr className={classes.tableRow}>
-                    <td data-title="">1</td>
-                    <td data-title="">Beximco</td>
-                    <td data-title="">1000</td>
-                    <td data-title="">Cash Payment</td>
-                    <td data-title="">01-01-2022</td>
-                </tr>
+                {purchases &&
+                    purchases
+                        // .filter((purchase) => purchase.name.toLowerCase().includes(searched))
+                        .splice(startIdx, endIdx)
+                        .map((purchase) => (
+                            <tr className={classes.tableRow} key={purchase.id}>
+                                <td data-title="">{purchase.id}</td>
+                                <td data-title="">XYZ</td>
+                                <td data-title="">{purchase.total_amount}</td>
+                                <td data-title="">{purchase.due_amount}</td>
+                                <td data-title="">{purchase.paid_amount}</td>
+                                <td data-title="">{purchase.purchase_date}</td>
+                            </tr>
+                        ))}
             </table>
+            <Pagination
+                setStartIdx={setStartIdx}
+                setEndIdx={setEndIdx}
+                page={purchases.filter((purchase) => purchase.id).length}
+            />
         </div>
     )
 }
