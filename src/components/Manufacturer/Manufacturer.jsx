bf97b446835f6220cd1d64338af7f8e68bdcef0e
morshedul-antor <morshedul.antor@northsouth.edu>
2022-02-05 01:42:48 +0600

    feat: new feature manufacturer list and form added

---
+++ src/components/Manufacturer/Manufacturer.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import ManufacturerForm from './ManufacturerForm/ManufacturerForm'
+import ManufacturerList from './ManufacturerList/ManufacturerList'
+
+export default function Manufacturer() {
+    const [manufacturers, setManufacturers] = useState([])
+    const [openForm, setOpenForm] = useState(false)
+
+    const auth = JSON.parse(localStorage.getItem('auth'))
+    const token = auth.token
+
+    useEffect(() => {
+        const controller = new AbortController()
+        const fetchData = async () => {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/manufacturers`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            const data = await response.json()
+            setManufacturers(data)
+        }
+        fetchData()
+        return () => {
+            fetchData()
+            controller.abort()
+        }
+    }, [token])
+    return (
+        <div>
+            {openForm && <ManufacturerForm setOpenForm={setOpenForm} />}
+            <ManufacturerList setOpenForm={setOpenForm} manufacturers={manufacturers} />
+        </div>
+    )
+}
