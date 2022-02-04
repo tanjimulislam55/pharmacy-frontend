4c8b0488261100a72d6e517a88213047704fcd01
morshedul-antor <morshedul.antor@northsouth.edu>
2022-02-05 01:44:40 +0600

    page: manufacturer page added

---
+++ src/pages/Manufacturer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Layout, Manufacturer } from '../components'
+
+export default function ManufacturerPage() {
+    return (
+        <Layout>
+            <Manufacturer />
+        </Layout>
+    )
+}
