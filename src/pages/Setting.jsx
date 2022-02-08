147d4c9707d81a74bdad46fbfdea7afade0b4ba7
morshedul-antor <morshedul.antor@northsouth.edu>
2022-02-08 17:49:10 +0600

    page: add setting page

---
+++ src/pages/Setting.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Layout, Setting } from '../components'
+
+export default function SettingPage() {
+    return (
+        <div>
+            <Layout>
+                <Setting />
+            </Layout>
+        </div>
+    )
+}
