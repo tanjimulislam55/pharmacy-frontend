40cbf69ea02a1c0ad9fd22ff9dfec41d7cae3ff3
morshedul-antor <morshedul.antor@northsouth.edu>
2022-02-17 17:23:38 +0600

    fetch: data fetch from backend

--- src/components/Grn/Grn.jsx
+++ src/components/Grn/Grn.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import GrnList from './GrnList/GrnList'
 
 export default function Grn() {
+    const [grns, setGrns] = useState([])
+
+    const auth = JSON.parse(localStorage.getItem('auth'))
+    const token = auth.token
+    const api = process.env.REACT_APP_API_URL
+
+    useEffect(() => {
+        const controller = new AbortController()
+        const fetchData = async () => {
+            const response = await fetch(`${api}/grns/`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            const data = await response.json()
+            setGrns(data)
+        }
+        fetchData()
+        return () => {
+            fetchData()
+            controller.abort()
+        }
+    }, [token, api])
+
     return (
         <div>
-            <GrnList />
+            <GrnList grns={grns} />
         </div>
     )
 }
