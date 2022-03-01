c6b980bd34c8c64bf68f1d6b8a845d3463e3bd46
morshedul-antor <morshedul.antor@northsouth.edu>
2022-03-01 18:05:44 +0600

    page: super admin page added

---
+++ src/pages/SuperAdmin.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SuperAdmin } from '../components'
+
+export default function SuperAdminPage() {
+    return (
+        <div>
+            <SuperAdmin />
+        </div>
+    )
+}
