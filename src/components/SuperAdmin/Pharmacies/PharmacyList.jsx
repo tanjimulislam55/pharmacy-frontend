3b68b40d6c12b852b3d8756bcb390c58a9ef561c
morshedul-antor <morshedul.antor@northsouth.edu>
2022-03-02 18:27:13 +0600

    add: active and waiting users list for admin approval added

--- src/components/SuperAdmin/Pharmacies/PharmacyList.jsx
+++ src/components/SuperAdmin/Pharmacies/PharmacyList.jsx
@@ -1,84 +1,132 @@
 import { faEdit, faEye, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import classes from './PharmacyList.module.css'
 
-export default function PharmacyList() {
+export default function PharmacyList({ pharmacyLists, headerList, activeStatus }) {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const auth = JSON.parse(localStorage.getItem('auth'))
+    const token = auth.token
+    const api = process.env.REACT_APP_API_URL
+
+    function activeId(id) {
+        fetch(`${api}/users/active/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        })
+    }
+
+    function removeId(id) {
+        fetch(`${api}/users/delete/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        })
+    }
+
+    const navigate = useNavigate()
+
     return (
         <div>
             <table className={classes.table}>
                 <thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Mobile</th>
-                        <th>Email</th>
-                        <th>Address</th>
-                        <th>Status</th>
-                        <th>Action</th>
+                        <th>ID</th>
+                        <th>{headerList.name}</th>
+                        <th>{headerList.mobile}</th>
+                        <th>{headerList.email}</th>
+                        <th>{headerList.status}</th>
+                        <th>{headerList.action}</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className={classes.alert}>
-                        <td>ABC Pharmacy</td>
-                        <td>01757000011</td>
-                        <td>abc@gmail.com</td>
-                        <td>Bashundhara</td>
-                        <td className={classes.status}>
-                            <span className={classes.active}>Active</span>
-                        </td>
-                        <td>
-                            <Link className={classes.icon} to="#">
-                                <FontAwesomeIcon icon={faEye} />
-                            </Link>
-                            <Link className={classes.icon} to="#">
-                                <FontAwesomeIcon icon={faEdit} />
-                            </Link>
-                            <Link className={classes.icon} to="#">
-                                <FontAwesomeIcon icon={faTrash} />
-                            </Link>
-                        </td>
-                    </tr>
-                    <tr className={classes.alert}>
-                        <td>EFG Pharmacy</td>
-                        <td>01757000022</td>
-                        <td>efg@gmail.com</td>
-                        <td>Banani</td>
-                        <td className={classes.status}>
-                            <span className={classes.waiting}>Waiting</span>
-                        </td>
-                        <td>
-                            <Link className={classes.icon} to="#">
-                                <FontAwesomeIcon icon={faEye} />
-                            </Link>
-                            <Link className={classes.icon} to="#">
-                                <FontAwesomeIcon icon={faEdit} />
-                            </Link>
-                            <Link className={classes.icon} to="#">
-                                <FontAwesomeIcon icon={faTrash} />
-                            </Link>
-                        </td>
-                    </tr>
-                    <tr className={classes.alert}>
-                        <td>XYZ Pharmacy</td>
-                        <td>01757000033</td>
-                        <td>xyz@gmail.com</td>
-                        <td>Mirpur</td>
-                        <td className={classes.status}>
-                            <span className={classes.delete}>Delete</span>
-                        </td>
-                        <td>
-                            <Link className={classes.icon} to="#">
-                                <FontAwesomeIcon icon={faEye} />
-                            </Link>
-                            <Link className={classes.icon} to="#">
-                                <FontAwesomeIcon icon={faEdit} />
-                            </Link>
-                            <Link className={classes.icon} to="#">
-                                <FontAwesomeIcon icon={faTrash} />
-                            </Link>
-                        </td>
-                    </tr>
+                    {pharmacyLists &&
+                        pharmacyLists.map((list) =>
+                            list.is_active === activeStatus.status && list.pharmacy !== null ? (
+                                <tr className={classes.alert} key={list.id}>
+                                    <td>{list.id}</td>
+                                    <td>{list.full_name}</td>
+                                    <td>{list.phone}</td>
+                                    <td>{list.email}</td>
+                                    <td className={classes.status}>
+                                        <span className={list.is_active === false ? classes.waiting : classes.active}>
+                                            {activeStatus.action}
+                                        </span>
+                                    </td>
+
+                                    <td>
+                                        <button className={classes.icon} onClick={() => setIsOpen(true)}>
+                                            <FontAwesomeIcon icon={faEye} />
+                                        </button>
+                                        <button className={classes.icon}>
+                                            <FontAwesomeIcon icon={faEdit} />
+                                        </button>
+                                        <button className={classes.icon}>
+                                            <FontAwesomeIcon icon={faTrash} />
+                                        </button>
+                                    </td>
+                                    {isOpen && (
+                                        <div key={() => list.id} className={classes.wrapper}>
+                                            <div className={classes.formWrapper}>
+                                                <div className={classes.close} onClick={() => setIsOpen(false)}>
+                                                    &times;
+                                                </div>
+
+                                                <table className={classes.table}>
+                                                    <thead className={classes.formHead}>
+                                                        <tr>
+                                                            <th>Pharmacy Name</th>
+                                                            <th>Trade License</th>
+                                                            <th>Address</th>
+                                                            <th>Area/Upazila</th>
+                                                            <th>City/District</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr className={classes.alert}>
+                                                            <td>{list.pharmacy.name}</td>
+                                                            <td>{list.pharmacy.trade_license}</td>
+                                                            <td>{list.pharmacy.area}</td>
+                                                            <td>{list.pharmacy.sub_district}</td>
+                                                            <td>{list.pharmacy.district}</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                                <div className={classes.buttonContainer}>
+                                                    {list.is_active === false ? (
+                                                        <button
+                                                            className={classes.button}
+                                                            onClick={() => {
+                                                                activeId(list.id)
+                                                                navigate('/super_admin')
+                                                            }}>
+                                                            Activate
+                                                        </button>
+                                                    ) : (
+                                                        ''
+                                                    )}
+                                                    <button
+                                                        className={classes.button}
+                                                        onClick={() => {
+                                                            removeId(list.id)
+                                                            navigate('/super_admin')
+                                                        }}>
+                                                        Delete
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )}
+                                </tr>
+                            ) : null
+                        )}
                 </tbody>
             </table>
         </div>
